226b817f5fcb4fd9e4a1d758379d2fed2c2674c6
Bola Ghaly <polaghaly63@gmail.com>
2022-04-06 01:47:13 -0400

    Update style.scss and ShowResults.tsx

--- src/components/ShowResults.tsx
+++ src/components/ShowResults.tsx
@@ -12,23 +12,25 @@ type Props = {
 };
 
 const GameOver: React.FC<Props> = (props: Props) => {
-  console.log("here 1 = ", props.answers);
+  // console.log("here 1 = ", props.answers);
 
   return (
-    <div>
+    <div className="resContainer">
       <h4>
         Score: {props.score} / {props.totalQuestions}
       </h4>
-      {props.userAnswers.map((e: any) => (
-        <div style={{ color: "white" }} key={e}>
-          <p dangerouslySetInnerHTML={{ __html: e.question }} />
-          {props.answers.map((answer) => (
-            <button value={answer} key={answer} className=" correctAns">
-              <span dangerouslySetInnerHTML={{ __html: answer }} />
-            </button>
-          ))}
-        </div>
-      ))}
+      <div className="ansContainer">
+        {props.userAnswers.map((e: any, index) => (
+          <div key={index} className="eachDiv">
+            <p dangerouslySetInnerHTML={{ __html: e.question }} />
+            {props.answers.map((answer) => (
+              <button key={answer} className=" correctAns" disabled={true}>
+                <span dangerouslySetInnerHTML={{ __html: answer }} />
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
