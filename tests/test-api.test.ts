99339f24435a9ea4e12fe7d7f4b270a38dd674cc
Bola Ghaly <polaghaly63@gmail.com>
2022-04-18 17:23:39 -0400

    Update test-api.test.ts

--- tests/test-api.test.ts
+++ tests/test-api.test.ts
@@ -5,9 +5,63 @@ import { expect } from "chai";
 import "mocha";
 
 let isEmpty: boolean;
-// let categories: string[] = [""];
+let categoriesNum: string[] = [
+  "",
+  "9",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "29",
+  "31",
+  "32",
+  "17",
+  "18",
+  "19",
+  "30",
+  "20",
+  "21",
+  "22",
+  "23",
+  "24",
+  "25",
+  "26",
+  "27",
+  "28",
+];
 
-describe("Testing API...", () => {  
+let categoriesName: string[] = [
+  "Any Category",
+  "General Knowledge",
+  "Entertainment: Books",
+  "Entertainment: Film",
+  "Entertainment: Music",
+  "Entertainment: Musicals & Theatres",
+  "Entertainment: Television",
+  "Entertainment: Video Games",
+  "Entertainment: Board Games",
+  "Entertainment: Comics",
+  "Entertainment: Japanese Anime & Manga",
+  "Entertainment: Cartoon & Animations",
+  "Science & Nature",
+  "Science: Computers",
+  "Science: Mathematics",
+  "Science: Gadgets",
+  "Mythology",
+  "Sports",
+  "Geography",
+  "History",
+  "Politics",
+  "Art",
+  "Celebrities",
+  "Animals",
+  "Vehicles",
+];
+
+describe("Testing API...", () => {
   it("should return 1-50 for the num of questions", async () => {
     for (let i = 0; i < 50; i++) {
       const response: any = `https://opentdb.com/api.php?amount=${i}`;
@@ -19,5 +73,7 @@ describe("Testing API...", () => {
     expect(isEmpty).to.be.false;
   });
 
-
+  it("should return true if categoriesNum and categoriesName string arrays are of the same length", () => {
+    expect(categoriesNum.length === categoriesName.length).to.be.true;
+  });
 });
